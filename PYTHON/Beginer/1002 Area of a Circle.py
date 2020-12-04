12d19bdc019eec9573f8d3e34e60537e61f6d03f
hridoyboss12 <75487507+hridoyboss12@users.noreply.github.com>
2020-12-04 20:43:35 +0600

    Python Solutions

---
+++ PYTHON/Beginer/1002 Area of a Circle.py
@@ -0,0 +1,4 @@
+import math
+R = float(input())
+A = 3.14159*math.pow(R,2)
+print("A=%.4f"%A)
---
+++ PYTHON/Beginer/1008 Salary.py
@@ -0,0 +1,7 @@
+NUMBER = int(input())
+HOUR = int(input())
+SALARY_PER_HOUR = float(input())
+SALARY = HOUR*SALARY_PER_HOUR
+print("NUMBER =",NUMBER)
+print("SALARY = U$ %.2f"%SALARY)
+

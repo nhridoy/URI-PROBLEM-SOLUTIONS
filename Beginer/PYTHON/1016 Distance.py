dce0934986313eaaa3cb792b0ce7d4c9517ce9dd
hridoyboss12 <75487507+hridoyboss12@users.noreply.github.com>
2020-12-29 13:18:44 +0600

    1015-1017

---
+++ Beginer/PYTHON/1016 Distance.py
@@ -0,0 +1,3 @@
+dis = int(input())
+minute = dis*2
+print(minute,"minutos")
---
+++ Beginer/PYTHON/1017 Fuel Spent.py
@@ -0,0 +1,6 @@
+hours = int(input())
+kmph = int(input())
+totalkm = hours*kmph
+fuel = totalkm/12
+frmt = format(fuel,'.3f')
+print(frmt)

7743017e6af5d1a9c2a4163e6eeb46ecab8d16a7
hridoyboss12 <75487507+hridoyboss12@users.noreply.github.com>
2021-01-13 14:38:43 +0600

    1035

--- Beginer/PYTHON/1035 Selection Test 1.py
+++ Beginer/PYTHON/1035 Selection Test 1.py
@@ -4,7 +4,7 @@ b = int(b)
 c = int(c)
 d = int(d)
 
-if b > c and d > a and c+d > a+b and c>0 and d>0 and a%2==0:
+if b > c and d > a and c+d > a+b and c > 0 and d > 0 and a % 2 == 0:
     print("Valores aceitos")
 else:
     print("Valores nao aceitos")

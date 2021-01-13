3f34189b209172f282fb89373a9957c41b6ce9c9
hridoyboss12 <75487507+hridoyboss12@users.noreply.github.com>
2021-01-13 15:34:17 +0600

    1072

---
+++ Beginer/PYTHON/1072 Interval 2.py
@@ -0,0 +1,12 @@
+n = int(input())
+interval = 0
+not_interval = 0
+
+for i in range(n):
+    num = int(input())
+    if 10 <= num <= 20:
+        interval = interval + 1
+    else:
+        not_interval = not_interval + 1
+print(interval, "in")
+print(not_interval, "out")

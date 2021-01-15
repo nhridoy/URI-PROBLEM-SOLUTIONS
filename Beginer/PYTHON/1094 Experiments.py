43538c30c148977b1dc93456cdf5016e2e21bb72
hridoyboss12 <75487507+hridoyboss12@users.noreply.github.com>
2021-01-15 11:42:16 +0600

    1094

---
+++ Beginer/PYTHON/1094 Experiments.py
@@ -0,0 +1,27 @@
+n = int(input())
+total_animal = 0
+rabbit = 0
+rat = 0
+frog = 0
+for i in range(n):
+    animal_number, name = input().split()
+    animal_number = int(animal_number)
+    total_animal = total_animal + animal_number
+    if name == 'C':
+        rabbit = rabbit + animal_number
+    elif name == 'R':
+        rat = rat + animal_number
+    elif name == 'S':
+        frog = frog + animal_number
+
+p_rabbit = (rabbit / total_animal) * 100
+p_rat = (rat / total_animal) * 100
+p_frog = (frog / total_animal) * 100
+
+print("Total:", total_animal, "cobaias")
+print("Total de coelhos:", rabbit)
+print("Total de ratos:", rat)
+print("Total de sapos:", frog)
+print("Percentual de coelhos: {:.2f} %".format(p_rabbit))
+print("Percentual de ratos: {:.2f} %".format(p_rat))
+print("Percentual de sapos: {:.2f} %".format(p_frog))

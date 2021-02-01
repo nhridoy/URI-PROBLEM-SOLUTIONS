2a2ae0449a163bec9af1f9a223e4c2fda95d1a3e
hridoyboss12 <75487507+hridoyboss12@users.noreply.github.com>
2021-02-01 19:55:09 +0600

    1132

---
+++ Beginer/PYTHON/1132 Multiples of 13.py
@@ -0,0 +1,21 @@
+x = int(input())
+y = int(input())
+s = 0
+if x < y:
+    for i in range(x, y+1):
+        if i % 13 != 0:
+            s += i
+
+    print(s)
+elif y < x:
+    for i in range(y, x + 1):
+        if i % 13 != 0:
+            s += i
+
+    print(s)
+else:
+    for i in range(y, x + 1):
+        if i % 13 != 0:
+            s += i
+
+    print(s)
